83e02a1c357c8b2305de1134e69c58e2e7dac722
cbaeucsd <102708712+cbaeucsd@users.noreply.github.com>
2023-02-10 16:08:06 -0800

    refactored Coordinates

--- app/src/main/java/com/example/cse110_team16_project/classes/Coordinates.java
+++ app/src/main/java/com/example/cse110_team16_project/classes/Coordinates.java
@@ -3,34 +3,38 @@ package com.example.cse110_team16_project.classes;
 import android.location.Location;
 import android.util.Pair;
 
-public class Coordinates {
-    private Pair<Double,Double> LongLat;
+import androidx.annotation.NonNull;
 
-    public Coordinates(double longitude, double latitude){
-        LongLat = new Pair<>(longitude,latitude);
+public class Coordinates extends Pair<Double,Double>{
+
+    public Coordinates(double latitude, double longitude){
+        super(latitude,longitude);
     }
 
     public Coordinates(Location location){
-        LongLat = new Pair<>(location.getLongitude(),location.getLatitude());
+        super(location.getLatitude(),location.getLongitude());
     }
 
     public Coordinates(String string){
-        String[] coords = string.split(",",2);
-        LongLat = new Pair<>(Double.valueOf(coords[0]),Double.valueOf(coords[1]));
+        super(Double.valueOf(string.split(",",2)[0]),
+                Double.valueOf(string.split(",",2)[1]));
     }
 
     public double getLongitude(){
-        return LongLat.first;
+        return this.second;
     }
 
     public double getLatitude(){
-        return LongLat.second;
+        return this.first;
     }
 
+    @Override
+    @NonNull
     public String toString(){
-        return Double.toString(LongLat.first) + "," + Double.toString(LongLat.second);
+        return this.first + "," + this.second;
     }
-
+    //https://www.movable-type.co.uk/scripts/latlong.html
+    //initial bearing
     public float bearingTo(Coordinates c){
         double lng1 = this.getLongitude();
         double lng2 = c.getLongitude();
@@ -39,8 +43,8 @@ public class Coordinates {
         double dLon = (lng2-lng1);
         double y = Math.sin(dLon) * Math.cos(lat2);
         double x = Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
-        double bearing = Math.toDegrees((Math.atan2(y, x)));
-        return (float) (360 - ((bearing + 360) % 360));
+        double theta = (Math.atan2(y, x));
+        return (float) ((Math.toDegrees(theta) + 360) % 360);
 
     }
 }
