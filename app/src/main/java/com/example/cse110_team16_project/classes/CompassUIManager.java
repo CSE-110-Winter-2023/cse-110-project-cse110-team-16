0f656fa21c22a67a0c37b881756487146fe3d74b
David Tran <tr.davidt@gmail.com>
2023-02-12 17:43:47 -0800

    Use fake coordinate instead of hard-coded home direction

--- app/src/main/java/com/example/cse110_team16_project/classes/CompassUIManager.java
+++ app/src/main/java/com/example/cse110_team16_project/classes/CompassUIManager.java
@@ -24,6 +24,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import io.reactivex.rxjava3.internal.operators.completable.CompletableOnErrorReturn;
+
 
 public class CompassUIManager {
     private static final String TAG = CompassUIManager.class.getSimpleName();
@@ -38,7 +40,7 @@ public class CompassUIManager {
     private List<Float> homeDirection;
     //private List<ImageView> homeIcons;
     private final int[] defaultColors = {0xFF000000,
-            0xFF003300,0xFF000033}; //black, kashmir green, midnight blue
+            0xFF003300, 0xFF000033}; //black, kashmir green, midnight blue
     //Initial colors of the text/icons for the first three homes
     //TODO: DECLARATION ABOVE IS DISGUSTING, MAKE TOLERABLE LATER
     private final ImageView compass;
@@ -47,7 +49,7 @@ public class CompassUIManager {
     private final HomeDirectionUpdater homeDirectionUpdater;
 
     public CompassUIManager(@NonNull UserTracker userTracker, @NonNull HomeDirectionUpdater homeDirectionTracker,
-                            ImageView compass, TextView sampleHome){
+                            ImageView compass, TextView sampleHome) {
         this.activity = userTracker.getActivity();
         this.userTracker = userTracker;
         this.homeDirectionUpdater = homeDirectionTracker;
@@ -56,7 +58,7 @@ public class CompassUIManager {
 
         DisplayMetrics displayMetrics = activity.getResources().getDisplayMetrics();
         float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
-        int dpRadius = (int)(dpWidth/SCREEN_PERCENTAGE);
+        int dpRadius = (int) (dpWidth / SCREEN_PERCENTAGE);
         //potentially use to make app work with different screen sizes
 
         populateHomeIcons(homeDirectionTracker.getHomes());
@@ -65,7 +67,7 @@ public class CompassUIManager {
                 this.future = backgroundThreadExecutor.submit(() ->
                 {
                     updateUI(direction, homeDirectionTracker.
-                        getLastKnownHomeDirectionsFromUser());
+                            getLastKnownHomeDirectionsFromUser());
                     return null;
                 }));
 
@@ -73,10 +75,10 @@ public class CompassUIManager {
     }
 
     //create views on the UI for each home
-    public void populateHomeIcons(List<Home> homes){
+    public void populateHomeIcons(List<Home> homes) {
 
         homeLabels = new ArrayList<>(homes.size());
-        for(int i = 0; i < homes.size(); i++){
+        for (int i = 0; i < homes.size(); i++) {
             TextView tv = new TextView(activity);
 
         }
@@ -85,36 +87,37 @@ public class CompassUIManager {
     //update the position of the view representing a home on the compass to the correct direction
 
     //params View to update, direction the home is from user in degrees from absolute north
-    public void updateIconDirection(TextView tv, Float homeDirection){
+    public void updateIconDirection(TextView tv, Float homeDirection) {
         ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) tv.getLayoutParams();
         layoutParams.circleAngle = homeDirection;
         tv.setLayoutParams(layoutParams);
     }
 
 
-    public void updateUI(float userDirection, List<Float> homeDirections){
+    public void updateUI(float userDirection, List<Float> homeDirections) {
         //if(Math.abs(userDirection - compass.getRotation()) < .15f) return;
 
         updateCompassDirection(userDirection);
         updateHomeIconDirections(homeDirections, userDirection);
     }
 
-    public void updateHomeIconDirections(List<Float> homeDirections, float userDirection){
-        /*
-        for(int i = 1; i < homeLabels.size(); i++){
-            updateIconDirection(homeLabels.get(i), homeDirections.get(i));
-        }
-        */
+    public void updateHomeIconDirections(List<Float> homeDirections, float userDirection) {
+//        for (int i = 0; i < homeLabels.size(); i++) {
+            // TODO: fake coordinates, should use robolectric test
+            Coordinates homePos = new Coordinates(32.734648946916835, -117.19090054085841);
+            Coordinates userPos = new Coordinates(32.8806731315563, -117.23402032381517);
+            final float homeDirection = userPos.bearingTo(homePos);
 
-        activity.runOnUiThread(()->{
-            // Set direction for sample home
-            updateIconDirection(sampleHome, (float) 170 - userDirection);
-        });
-
+            // final float homeDirection = homeDirections.get(i);
+            activity.runOnUiThread(() -> {
+                // Set direction for sample home
+                updateIconDirection(sampleHome, homeDirection - userDirection);
+            });
+//        }
     }
 
     //given userDirection in degrees, changes compass to face correct direction
-    public void updateCompassDirection(float userDirection){
+    public void updateCompassDirection(float userDirection) {
 
         activity.runOnUiThread(() -> {
             //compass.startAnimation(ra);
