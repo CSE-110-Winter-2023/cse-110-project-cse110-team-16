046b101a8a6e98f0e1237a2d9ffe83b0cca393ee
Shree-G <shree.hridai@gmail.com>
2023-02-15 15:07:41 -0800

    implemented persistence of coords as floats

--- app/src/main/java/com/example/cse110_team16_project/AddHomeLocations.java
+++ app/src/main/java/com/example/cse110_team16_project/AddHomeLocations.java
@@ -2,14 +2,25 @@ package com.example.cse110_team16_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddHomeLocations extends AppCompatActivity {
-    public double yourHomeX, yourHomeY;
+//    public double yourHomeX, yourHomeY;
+//
+//    public double getYourHomeX(){
+//        return yourHomeX;
+//    }
+//
+//    public double getYourHomeY(){
+//        return yourHomeY;
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +34,11 @@ public class AddHomeLocations extends AppCompatActivity {
         super.onDestroy();
         saveProfile();
 
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        String home = preferences.getString("home", "");
-        double[] yourHomeArr = storeCoords(home);
-        yourHomeX = yourHomeArr[0];
-        yourHomeY = yourHomeArr[1];
+//        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+//        String home = preferences.getString("home", "");
+//        double[] yourHomeArr = storeCoords(home);
+//        yourHomeX = yourHomeArr[0];
+//        yourHomeY = yourHomeArr[1];
     }
 
 //    public void loadProfile() {
@@ -47,69 +58,47 @@ public class AddHomeLocations extends AppCompatActivity {
 //        friendHomeView.setText(friendHome);
 //    }
 
-    public double[] storeCoords(String coords) {
+    public float[] storeCoords(String coords) {
         coords = coords.substring(1,coords.length() - 1);
         String[] arrOfStr = coords.split(", ", 2);
 
-//        if(arrOfStr.length < 2 ) {
-//            // stop and pass error!
-//            //Utilities.showAlert(this, "Must pass coordinates in the form (x-coord, y-coord)");
-//            return null;
-//        }
+        float xCoord = Float.parseFloat(arrOfStr[0]);
+        float yCoord = Float.parseFloat(arrOfStr[1]);
 
-        double xCoord = Double.parseDouble(arrOfStr[0]);
-        double yCoord = Double.parseDouble(arrOfStr[1]);
-
-        double[] toRet = new double[]{xCoord, yCoord};
+        float[] toRet = new float[]{xCoord, yCoord};
         return toRet;
 
     }
 
     public void saveProfile() {
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences preferences = getSharedPreferences("HomeLoc", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
 
         TextView homeView = findViewById(R.id.yourHomeField);
-        editor.putString("home", homeView.getText().toString());
+        float[] yourHomeFloats = this.storeCoords(homeView.getText().toString());
+        Log.d("TESTFLOAT", yourHomeFloats[0] + "");
+        Log.d("TESTFLOAT", yourHomeFloats[1] + "");
+        editor.putFloat("yourHomeX", yourHomeFloats[0]);
+        editor.putFloat("yourHomeY", yourHomeFloats[1]);
+        //editor.putString("home", homeView.getText().toString());
 
-        TextView famHomeView = findViewById(R.id.yourFamHomeField);
-        editor.putString("famHome", famHomeView.getText().toString());
-
-        TextView friendHomeView = findViewById(R.id.yourBestFriendHomeField);
-        editor.putString("friendHome", friendHomeView.getText().toString());
+//        TextView famHomeView = findViewById(R.id.yourFamHomeField);
+//        editor.putString("famHome", famHomeView.getText().toString());
+//
+//        TextView friendHomeView = findViewById(R.id.yourBestFriendHomeField);
+//        editor.putString("friendHome", friendHomeView.getText().toString());
 
         editor.apply();
     }
 
     public void onSubmitClicked(View view) {
-
-        //Utilities.showAlert(this, "Dummy alert");
-
-        //checkDataEntered();
-        // storing coordinates of your home in public double var
-
         finish();
-
-
     }
 
-//    private void checkDataEntered() {
-//        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-//
-//        // at least one field should be filled for AddHomeLoc
-//        String home = preferences.getString("home", "");
-//        String famHome = preferences.getString("famHome", "");
-//        String friendHome = preferences.getString("friendHome", "");
-//
-//        if(!(home.length() > 0 || famHome.length() > 0 || friendHome.length() > 0)){
-//            //pass error!
-//            Toast t = Toast.makeText(this, "Please fill in at least one coordinate field!", Toast.LENGTH_LONG);
-//            t.show();
-//        }
-//
-//        // must provide in format (x-coord, y-coord)
-//
-//        // x-coord and y-coord must be valid doubles
-//
-//    }
+    public void onSanityClicked(View view) {
+        saveProfile();
+
+        Intent intent = new Intent(this, SanityActivity.class);
+        startActivity(intent);
+    }
 }
