3f883f53f9fad01fb49544734604c5f09d719ea4
ericwpei <epei@ucsd.edu>
2023-02-15 15:27:04 -0800

    got rid of story 2 code

--- app/src/main/java/com/example/cse110_team16_project/MainActivity.java
+++ app/src/main/java/com/example/cse110_team16_project/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this,CompassActivity.class);
-        startActivity(intent);
+        // Intent intent = new Intent(this,CompassActivity.class);
+        // startActivity(intent);
     }
 
     public void goToAddHomeLocations(View view) {
