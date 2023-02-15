8569a190664c680e77519c9697061894ba204e81
Shree-G <shree.hridai@gmail.com>
2023-02-15 15:07:41 -0800

    implemented persistence of coords as floats

---
+++ app/src/test/java/com/example/cse110_team16_project/AddHomeLocTest.java
@@ -0,0 +1,33 @@
+//package com.example.cse110_team16_project;
+//
+//import android.widget.Button;
+//import android.widget.EditText;
+//import static org.junit.Assert.assertEquals;
+//
+//import androidx.lifecycle.Lifecycle;
+//import androidx.test.core.app.ActivityScenario;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.robolectric.RobolectricTestRunner;
+//
+//@RunWith(RobolectricTestRunner.class)
+//public class AddHomeLocTest {
+//
+//    @Test
+////    public void test_storage_doubles() {
+////        ActivityScenario scenario = ActivityScenario.launch(AddHomeLocations.class);
+////        scenario.moveToState(Lifecycle.State.CREATED);
+////        scenario.moveToState(Lifecycle.State.STARTED);
+////
+////        scenario.onActivity(activity -> {
+////            EditText yourHome = activity.findViewById(R.id.yourHomeField);
+////            yourHome.setText("(12.1231323,67.8903457)");
+////
+////            Button submit_btn = activity.findViewById(R.id.submitBtn);
+////            submit_btn.performClick();
+////
+////            assertEquals(12.1231323, AddHomeLocations.getYourHomeX(), 0.1);
+////        });
+////    }
+//}
