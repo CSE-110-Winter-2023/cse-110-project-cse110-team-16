1f6ff83b87513dbc2e14e9e2a4308c8f99d03d13
ericwpei <epei@ucsd.edu>
2023-02-15 19:58:49 -0800

    added persistence for family home label

--- app/src/main/java/com/example/cse110_team16_project/AddLabels.java
+++ app/src/main/java/com/example/cse110_team16_project/AddLabels.java
@@ -2,7 +2,11 @@ package com.example.cse110_team16_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AddLabels extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class AddLabels extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_labels);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        saveProfile();
+    }
+
+    public void saveProfile() {
+        SharedPreferences preferences = getSharedPreferences("FamHomeLabel", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        TextView famView = findViewById(R.id.famHomeLabelField);
+        editor.putString("famLabel", famView.getText().toString());
+
+        editor.apply();
+    }
+
+    public void onSubmitClicked(View view) {
+        finish();
     }
 }
