f7d57ab1918fa4c7d8bace3da45d764cf7d5dd05
Victor Ren <victorren2002@gmail.com>
2023-03-03 11:45:28 -0800

    Finished setting up xml file for AddNameActivity

---
+++ app/src/main/java/com/example/cse110_team16_project/AddNameActivity.java
@@ -0,0 +1,18 @@
+package com.example.cse110_team16_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AddNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_name);
+    }
+
+    public void onSubmitClicked(View view) {
+    }
+}
