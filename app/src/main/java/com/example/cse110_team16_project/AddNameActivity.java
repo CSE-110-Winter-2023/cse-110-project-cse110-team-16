d1f5c1ee3a838f3d534a4333811e2a2ead6f95d4
Victor Ren <victorren2002@gmail.com>
2023-03-05 16:11:30 -0800

    onSubmitClicked updates label, private_code, public_code

--- app/src/main/java/com/example/cse110_team16_project/AddNameActivity.java
+++ app/src/main/java/com/example/cse110_team16_project/AddNameActivity.java
@@ -2,8 +2,12 @@ package com.example.cse110_team16_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.UUID;
 
 public class AddNameActivity extends AppCompatActivity {
 
@@ -14,5 +18,17 @@ public class AddNameActivity extends AppCompatActivity {
     }
 
     public void onSubmitClicked(View view) {
+        String name = findViewById(R.id.YourNameField).toString();
+        String private_code = UUID.randomUUID().toString();
+        String public_code = UUID.randomUUID().toString();
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString("label", name);
+        editor.putString("private_code", private_code);
+        editor.putString("public_code", public_code);
+
+        editor.apply();
     }
 }
