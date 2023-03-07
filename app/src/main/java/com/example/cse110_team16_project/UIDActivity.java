1427e0ada3e80cde814ec8969cc61976b401b8d6
Victor Ren <victorren2002@gmail.com>
2023-03-06 21:11:15 -0800

    Created UIDActivity

---
+++ app/src/main/java/com/example/cse110_team16_project/UIDActivity.java
@@ -0,0 +1,14 @@
+package com.example.cse110_team16_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UIDActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uidactivity);
+    }
+}
