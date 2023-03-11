6f46a136513b53ac08ad2a62ae5f38d7f39bb298
David Tran <tr.davidt@gmail.com>
2023-03-11 12:17:28 -0800

    count mins gps lost when start app with no gps signal + make status UI sound like requirements

--- app/src/main/java/com/example/cse110_team16_project/classes/GPSstatus.java
+++ app/src/main/java/com/example/cse110_team16_project/classes/GPSstatus.java
@@ -16,69 +16,77 @@ import java.util.Objects;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class GPSstatus{
+public class GPSstatus {
     private LiveData<Location> location;
-
-    private View view;
+    private View statusDot;
     private TextView gpsText;
+    private long initTime;
 
     private static final int TIME_THRESHOLD = 60000;
     private static final int ONE_MIN = 60000;
     private static final int ONE_HOUR = 3600000;
 
-    public GPSstatus(LiveData<Location> loc, View v, TextView gt){
+    public GPSstatus(LiveData<Location> loc, View v, TextView gt) {
         this.location = loc;
-        this.view = v;
+        this.statusDot = v;
         this.gpsText = gt;
+        this.initTime = 0;
     }
 
-    private long getLocationAge(){
+    private long getLocationAge() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            return Objects.requireNonNull(this.location.getValue()).getElapsedRealtimeAgeMillis();
+            Location currLoc = this.location.getValue();
+            if (currLoc != null) {
+                return currLoc.getElapsedRealtimeAgeMillis();
+            }
+            else {
+                Log.d("GPS", "No data yet!");
+                long initTimeNow = initTime;
+                initTime += ONE_MIN / 60;
+                return initTimeNow;
+            }
         }
         return 0;
     }
 
-    public boolean isLocationLive(){
+    public boolean isLocationLive() {
         Log.d("GPS", Long.toString(getLocationAge()));
-       return getLocationAge() < TIME_THRESHOLD;
+        return getLocationAge() < TIME_THRESHOLD;
     }
 
-    public void updateGPSLostTime(){
+    public void updateGPSLostTime() {
         long locationAge = getLocationAge();
-        if(locationAge < ONE_HOUR){
-            this.gpsText.setText("GPS Lost " + Converters.milisecToMins(locationAge) + "m");
+        if (locationAge < ONE_HOUR) {
+            this.gpsText.setText(Converters.milisecToMins(locationAge) + "m");
+        } else {
+            this.gpsText.setText(Converters.milisecToHours(locationAge) + "h");
         }
-        else{
-            this.gpsText.setText("GPS Lost " + Converters.milisecToHours(locationAge) + "hr");
-        }
-
     }
-    public void trackGPSStatus(){
+
+    public void trackGPSStatus() {
         var executor = Executors.newSingleThreadScheduledExecutor();
         executor.scheduleAtFixedRate(() -> {
-            try{
-                if (isLocationLive()){
+            try {
+                if (isLocationLive() && this.location.getValue() != null) {
                     this.setGreen();
-                    this.gpsText.setText("GPS ON");
+                    this.gpsText.setText("");
                     Log.d("GPS", "GPS status set to green");
                 } else {
                     this.setRed();
                     updateGPSLostTime();
                     Log.d("GPS", "GPS status set to red");
                 }
-            } catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-
-        },0,3, TimeUnit.SECONDS);
+        }, 0, 3, TimeUnit.SECONDS);
     }
 
-    private void setGreen(){
-        this.view.setBackgroundResource(R.drawable.gps_green);
+    private void setGreen() {
+        this.statusDot.setBackgroundResource(R.drawable.gps_green);
     }
 
-    private void setRed(){
-        this.view.setBackgroundResource(R.drawable.gps_red);
+    private void setRed() {
+        this.statusDot.setBackgroundResource(R.drawable.gps_red);
     }
 }
