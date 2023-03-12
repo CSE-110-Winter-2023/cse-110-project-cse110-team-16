a75c9e91899de1f48e28090babe4817ac8651a59
CJWAKE998 <97641362+CJWAKE998@users.noreply.github.com>
2023-03-12 14:20:33 -0700

    So far with UserUIAdapter, still working on observing orientation

--- app/src/main/java/com/example/cse110_team16_project/CompassActivity.java
+++ app/src/main/java/com/example/cse110_team16_project/CompassActivity.java
@@ -20,6 +20,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.cse110_team16_project.classes.CompassUIManager;
 import com.example.cse110_team16_project.classes.CompassViewModel;
--- app/src/main/java/com/example/cse110_team16_project/classes/UserUIAdapter.java
+++ app/src/main/java/com/example/cse110_team16_project/classes/UserUIAdapter.java
@@ -12,6 +12,7 @@ import androidx.lifecycle.LiveData;
 
 import com.example.cse110_team16_project.R;
 import com.example.cse110_team16_project.Units.Degrees;
+import com.example.cse110_team16_project.Units.Radians;
 
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -26,20 +27,32 @@ public class UserUIAdapter{
 
 
 
-
+    private Future<Void> future;
     Activity activity;
     private List<String> friendLabels;
     private LiveData<List<Double>> friendDistances;
     private LiveData<List<Degrees>> friendOrientation;
     private List<TextView> friends;
+
+    private OrientationService orientationService;
+
     public UserUIAdapter(Activity activity, @NonNull LiveData<List<Double>> friendDistances,
                          @NonNull LiveData<List<Degrees>> friendOrientation, List<String> friendLabels,
-                         DistanceUpdater distanceUpdater, AbsoluteDirectionUpdater absoluteDirectionUpdater){
-
+                         List<TextView> friends, OrientationService orientationService){
+        this.friends = friends;
         this.activity = activity;
         this.friendDistances = friendDistances;
         this.friendOrientation = friendOrientation;
         this.friendLabels = friendLabels;
+        populateFriendLabels(friendLabels, friends);
+
+
+                friendDistances.observe((LifecycleOwner) activity,
+                directions -> this.future = backgroundThreadExectur.submit(() -> {
+                    updateUI(Converters.RadiansToDegrees(orientationService.getOrientation()), friendOrientation);
+                    return null;
+                })
+        );
 
 
     }
@@ -57,20 +70,33 @@ public class UserUIAdapter{
         }
     }
 
-    public void updateUI(){
-
+    public void updateUI(Degrees userDirection, List<Degrees> friendOrientation, List<Double> friendDistances){
+        if(friendOrientation.size() == 0){
+            return;
+        }
+        updateIconDirections(userDirection, friends, friendOrientation);
+        updateIconDistances(friends, friendDistances);
     }
 
     //update the position of the view representing a entity on the compass to the correct direction
     //params View to update, relative direction the entity is from user in degrees
-    public void updateIconDirection(TextView tv, Degrees entityDirection) {
-        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) tv.getLayoutParams();
-        layoutParams.circleAngle = (float) entityDirection.getDegrees();
-        activity.runOnUiThread(() -> tv.setLayoutParams(layoutParams));
+    public void updateIconDirections(Degrees userDirection, List<TextView> friendViews, List<Degrees> friendOrientation) {
+        for(int i = 0 ; i < friendOrientation.size() ; i++){
+            friendOrientation.set(i, Degrees.subtractDegrees(friendOrientation.get(i),userDirection));
+        }
+        for(int i = 0 ; i < friendViews.size() ; i++) {
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) friendViews.get(i).getLayoutParams();
+            layoutParams.circleAngle = (float) friendOrientation.get(i).getDegrees();
+            friendViews.get(i).setLayoutParams(layoutParams);
+        }
     }
 
-    public void updateIconDistance(){
-
+    public void updateIconDistances(List<TextView> friendViews, List<Double> friendDistances){
+        for(int i = 0 ; i < friendViews.size() ; i++) {
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) friendViews.get(i).getLayoutParams();
+            layoutParams.circleRadius = friendDistances.get(i).intValue();
+            friendViews.get(i).setLayoutParams(layoutParams);
+        }
     }
 
 
