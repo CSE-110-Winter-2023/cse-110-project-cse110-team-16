a2a2e540dd8313aa301166cc364851583a29fef7
cbaeucsd <102708712+cbaeucsd@users.noreply.github.com>
2023-03-16 04:53:50 -0700

    pretty good working vertical displacement

--- app/src/main/java/com/example/cse110_team16_project/IconCollisionHandler.java
+++ app/src/main/java/com/example/cse110_team16_project/IconCollisionHandler.java
@@ -15,8 +15,8 @@ import java.util.List;
 
 public class IconCollisionHandler {
 
-    private final int HEIGHT = 20;
-    private final int WIDTH_TOP_DOWN_CHECK = 60;
+    private final int HEIGHT = 70;
+    private final int WIDTH_TOP_DOWN_CHECK = 40;
 
     private final int WIDTH_LEFT_RIGHT_CHECK = 120;
     private Degrees userDirection;
@@ -45,20 +45,26 @@ public class IconCollisionHandler {
     }
 
     public void adjustTopDown(List<Rect> rects){
-        for(int i = 1; i < rects.size(); i++){
-            if(ignore.get(i) == true) continue;
-            for(int j = 0; j < i; j++){
-                if(ignore.get(j) == true) continue;
-                Rect rect1 = rects.get(i);
-                Rect rect2 = rects.get(j);
-                if(Rect.intersects(rect1,rect2) || rect1.equals(rect2)) {
-                    if(rect1.top > rect2.top){
-                        rect1.offset((HEIGHT-rect1.top-rect2.top)/2,0);
-                        rect2.offset(-HEIGHT+(rect1.top-rect2.top)/2,0);
-                    }
-                    else {
-                        rect1.offset(-HEIGHT+(rect1.top-rect2.top)/2,0);
-                        rect2.offset(HEIGHT-(rect1.top-rect2.top)/2,0);
+        boolean adjusted = false;
+        int tries = 0;
+        while (!adjusted && tries < 3) {
+            tries++;
+            adjusted = true;
+            for (int i = 1; i < rects.size(); i++) {
+                if (ignore.get(i)) continue;
+                for (int j = 0; j < i; j++) {
+                    if (ignore.get(j)) continue;
+                    Rect rect1 = rects.get(i);
+                    Rect rect2 = rects.get(j);
+                    if (Rect.intersects(rect1, rect2)) {
+                        if (rect1.bottom > rect2.bottom) {
+                            rect1.offset(0, HEIGHT / 2 - (rect1.bottom - rect2.bottom));
+                            rect2.offset(0, -HEIGHT / 2 + (rect1.bottom - rect2.bottom));
+                        } else {
+                            rect1.offset(0, -HEIGHT / 2 + (-rect1.bottom + rect2.bottom));
+                            rect2.offset(0, HEIGHT / 2 - (-rect1.bottom + rect2.bottom));
+                        }
+                        adjusted = false;
                     }
                 }
             }
@@ -68,7 +74,7 @@ public class IconCollisionHandler {
     public Rect calculateTopDownCheckRectangle(Double dist1, Degrees deg1){
         int x1 = (int) (dist1*Math.cos(Converters.DegreesToRadians(deg1).getRadians()));
         int y1 = (int) (dist1*Math.sin(Converters.DegreesToRadians(deg1).getRadians()));
-        return new Rect(x1-WIDTH_TOP_DOWN_CHECK/2, y1+HEIGHT/2, x1+WIDTH_TOP_DOWN_CHECK/2, y1-HEIGHT/2);
+        return new Rect(x1-WIDTH_TOP_DOWN_CHECK/2, y1-HEIGHT/2, x1+WIDTH_TOP_DOWN_CHECK/2, y1+HEIGHT/2);
     }
     public boolean checkTopDownCollision(Rect rect1, Rect rect2){
         return Rect.intersects(rect1,rect2);
@@ -89,7 +95,7 @@ public class IconCollisionHandler {
         adjustedAngles = new ArrayList<>(friendDistances.size());
         adjustedRadius = new ArrayList<>(friendDistances.size());
         for(int i = 0; i < rects.size(); i++){
-            if(ignore.get(i) == true){
+            if(ignore.get(i)){
                 adjustedRadius.add(friendDistances.get(i));
                 adjustedAngles.add(Degrees.subtractDegrees(friendOrientation.get(i),userDirection));
             }
@@ -97,7 +103,7 @@ public class IconCollisionHandler {
                 int x = rects.get(i).centerX();
                 int y = rects.get(i).centerY();
                 adjustedRadius.add(Math.sqrt(x * x + y * y));
-                adjustedAngles.add(new Degrees(Math.toDegrees(Math.atan2(y, x))));
+                adjustedAngles.add(new Degrees(Math.toDegrees(Math.atan2(y, x))+90));
             }
         }
     }
